ff7611d4aea757388ff0dcac335ae1118785471f
Snigdha Sanat <snigdhajyoti.sanat@verizon.com>
2017-11-08 16:40:33 +0530

    Few changes in main js

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-var config = require('./config')
+var config = require("./config")
+//var credentials=require("./credentials")
 
-//var multer  = require('multer')
-//var upload = multer({ dest: 'uploads/' });
-
-var AWS = require('aws-sdk');
-AWS.config.region = config.region;
-
-var uuid = require('node-uuid');
-var fs = require('fs-extra');
-var path = require('path');
+//var multer  = require("multer")
+//var upload = multer({ dest: "uploads/" });
 
 
-//app.use(express.static('public'));
+var AWS = require("aws-sdk");
+//AWS.config.region = config.region;
+AWS.config.update({region: "us-east-1"});
 
-var rekognition = new AWS.Rekognition({region: config.region});
+//console.log(AWS.config);
+var accessKeyID=AWS.config.credentials.accessKeyId;
+var secretAccessKey=AWS.config.credentials.secretAccessKey;
+//console.log(accessKeyID);
+//console.log(secretAccessKey);
+
+var uuid = require("node-uuid");
+var fs = require("fs-extra");
+var path = require("path");
+
+
+//app.use(express.static("public"));
+
+var recognition = new AWS.Rekognition({region: config.region});
 var s3 = new AWS.S3({region: config.region}); 
 
 
@@ -25,24 +34,37 @@ var s3 = new AWS.S3({region: config.region});
 
 	var params={	 	
 	Image: { 
-    //"Bytes": bitmap,
+    
     S3Object: {
-      Bucket: "detactface",
+      Bucket: "detectface",
       Name: "FB_IMG_1504715128043.jpg"
-      //version: "S3ObjectVersion",
-    }
-  }
-  //attributes: ["DEFAULT"] // accepts DEFAULT, ALL
+     }
+  },
+  Attributes: [
+    "ALL"
+  ]
 	};
+
 	
-	var http = require("http");
+recognition.detectFaces(params, function(error, response) {
+ if (error) console.log(error, error.stack); // an error occurred
+ else 
+ {
+  console.log("-------- START: Facial analysis --------");
+  var faceDetails = response.FaceDetails[0];
+  console.log("Person is smiling :"+faceDetails.Smile.Value);
+  console.log("Person is wearing sunglasses :"+faceDetails.Sunglasses.Value);
+  console.log("Person gender :"+faceDetails.Gender.Value);
+  console.log("Person has beard :"+faceDetails.Beard.Value);
+  console.log("Person has mustache :"+faceDetails.Mustache.Value);
+  console.log("Person has EyesOpen :"+faceDetails.EyesOpen.Value);
+  console.log("-------- END: Facial analysis --------");
+  console.log("\n");
+ }
+});
 
 
-rekognition.detectFaces(params, function(err, data) {
-	 if (err) console.log(err, err.stack); // an error occurred
-else     console.log(data);           // successful response
-	}
-	);
+
    
 
 
